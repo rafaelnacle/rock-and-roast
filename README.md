66918c78e5729809082f37b9bcc53a3734104f09
Rafael Nacle <54647722+rafaelnacle@users.noreply.github.com>
2023-10-26 14:09:18 -0300

    html render for the products

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Rock & Roast
+
+Restaurant menu app for another solo project from Scrimba
+
+## What is the specs?
+- Create the layout based on the figma project
+- Render the menu options using the data with JavaScript
+- Be able to add/remove items
+- Have a payment modal with compulsory form inputs
+
+## Stretch goals
+- We can change the theme and colors etc
+- Offer a "meal deal" discount
+- Allow users to rate their experience
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,28 @@
 import { menuArray } from "./data.js"
 
+const productSectionEl = document.getElementById('products')
+
+
 function getProductList() {
   menuArray.forEach((product) => {
-    console.log(product)
+    productSectionEl.innerHTML += `
+      <div class="product-container">
+        <div class="prod-wrapper">
+          <p class="prod-emoji">${product.emoji}</p>
+          <div class="prod-info">
+            <h2>${product.name}</h2>
+            <p class="prod-description">${product.ingredients}</p>
+            <h3>${product.price} $</h3>
+          </div>
+        </div>
+        <p class="prod-add-btn" id="prod-add-btn">+</p>
+      </div>
+    `
   })
+
+  const prodAddBtn = document.getElementById("prod-add-btn")
+
+  prodAddBtn.addEventListener('click', () => console.log('click'))
 }
 
 function render() {
